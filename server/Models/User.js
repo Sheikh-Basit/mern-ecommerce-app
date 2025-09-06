2a8d3dfe31aa373bdc3dc793d338ac5021045c74
Sheikh Abdul Basit <ab2299631@gmail.com>
2025-09-06 20:09:41 +0500

    Update the user model (Schema)

--- server/Models/User.js
+++ server/Models/User.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
   username: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
+  role:     { type: String, enum: ["user", "admin"], default: "user" },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
   date: { type: Date, default: Date.now },

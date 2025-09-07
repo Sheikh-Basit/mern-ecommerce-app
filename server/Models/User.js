7a3b6af3501f70cf273e64b0f9849975f8c821ba
Sheikh Abdul Basit <ab2299631@gmail.com>
2025-09-07 16:12:39 +0500

    update the user model (schema)

--- server/Models/User.js
+++ server/Models/User.js
@@ -5,11 +5,10 @@ const userSchema = new Schema({
   username: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
-  role:     { type: String, enum: ["user", "admin"], default: "user" },
+  role:     { type: String, enum: ["user", "admin"], default: "admin" },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
-  date: { type: Date, default: Date.now },
-});
+}, { timestamps: true });
 
 // Create Model
 const User = mongoose.model('User', userSchema);

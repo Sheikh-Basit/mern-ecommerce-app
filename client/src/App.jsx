b906da4e11942eeafac1a3f7e5bf139f85582654
Sheikh Abdul Basit <ab2299631@gmail.com>
2025-09-17 21:52:55 +0500

    update frondend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import react from 'react'
-// import {BrowserRouter as Router, Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import MainPage from './Admin/MainPage'
 
 function App() {
  
 
   return (
+    <Router>
     <div className='container-fluid flex'>
+      <h1>this is h1 tag</h1>
     <MainPage/>
     </div>
+    </Router>
   )
 }
 

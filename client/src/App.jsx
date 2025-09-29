1e85005c15e0aa68a201c2d6712b62d72374ca05
Sheikh Abdul Basit <ab2299631@gmail.com>
2025-09-29 10:17:43 +0500

    Delete the previous practice file in frondend and start working on the real project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import react from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import MainPage from './Admin/MainPage'
 
 function App() {
  
@@ -9,7 +8,7 @@ function App() {
     <Router>
     <div className='container-fluid flex'>
       <h1>This is just for testing</h1>
-    <MainPage/>
+    
     </div>
     </Router>
   )

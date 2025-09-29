98889a027f6fbb19636ee24b25b813488bf143c2
Sheikh Abdul Basit <ab2299631@gmail.com>
2025-09-29 12:56:55 +0500

    import cors in the backend

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors';
 import dotenv from 'dotenv';
 import DatabaseConn from './config/db.js';
 import userRoutes from "./Routes/auth.js";
@@ -16,6 +17,8 @@ const app = express();
 
 // middleware
 app.use(express.json());
+
+app.use(cors());
 
 // Routes
 app.use("/auth",userRoutes);

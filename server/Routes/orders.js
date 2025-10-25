777757efe49903f2401d821a0c392b39ca258bb7
Sheikh Abdul Basit <ab2299631@gmail.com>
2025-10-25 18:56:24 +0500

    Create a new route to update the order status

--- server/Routes/orders.js
+++ server/Routes/orders.js
@@ -13,6 +13,21 @@ router.get('/getAllOrders', fetchUser, isAdmin, async (req, res) => {
     }
 })
 
+// 2 => Update order status (Access only Admin) using the PUT request: http://localhost:3000/orders/updateOrderStatus/:id
+router.put("/updateOrderStatus/:id", async (req, res) => {
+  try {
+    const order = await Orders.findByIdAndUpdate(
+      req.params.id,
+      { "status": req.body.status },
+      { new: true }
+    );
+    res.status(200).json({message:"Order Status Updated", order});
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update status" });
+  }
+});
+
+
 
 
 export default router;
